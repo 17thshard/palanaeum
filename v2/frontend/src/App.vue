2aafd7f0eb8ae2654543d4226867de700a95d5f6
Marvin Rösch <marvinroesch99@gmail.com>
2019-10-19 20:35:47 +0200

    Set up very *basic* layout for Vue.js app

--- v2/frontend/src/App.vue
+++ v2/frontend/src/App.vue
@@ -1,32 +1,33 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <main id="app">
+    <Header></Header>
+    <router-view />
+  </main>
 </template>
 
+<script>
+import Header from '@/components/layout/Header.vue';
+
+export default {
+  name: 'App',
+  components: { Header },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+body {
+  max-width: $max-body-width !important;
+  background-color: $body-background;
+  font-size: $base-font-size;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
+  border-style: solid;
+  border-width: 1px;
+  border-color: #003a52;
+  background-color: #fcfcfc;
 }
 </style>
---
+++ v2/frontend/src/components/layout/Header.vue
@@ -0,0 +1,16 @@
+<template functional>
+  <header>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style lang="scss">
+.header {
+
+}
+</style>
