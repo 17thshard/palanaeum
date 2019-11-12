b3378e2dcf000f8ac9467d8dcd6e4d67098a4303
Hilton Shumway <hillshum@gmail.com>
2019-11-11 23:38:45 -0600

    Basic work on pausing other players

--- palanaeum/static/palanaeum/js/mini_audio_player.js
+++ palanaeum/static/palanaeum/js/mini_audio_player.js
@@ -3,6 +3,9 @@ function mini_player_clicked() {
     var audio_player = button.siblings('audio');
 
     if (audio_player.prop('paused')) {
+        var article = button.parents('#entries')
+        var other_players = article.find('audio')
+        other_players.each(mini_player_pause)
         audio_player.trigger('play');
         button.removeClass('fa-play').addClass('fa-pause');
     } else {
@@ -14,6 +17,14 @@ function mini_player_clicked() {
         audio_player.prop("currentTime", 0);
     });
     return false;
+}
+
+function mini_player_pause() {
+    var player = $(this)
+    var button = player.siblings('button')
+    player.trigger('pause')
+    button.removeClass('fa-pause').addClass('fa-play')
+
 }
 
 $(function(){
