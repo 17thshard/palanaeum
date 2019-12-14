834678d950b5b623a0a842dc022d60832bc8700b
Marvin Rösch <marvinroesch99@gmail.com>
2019-12-14 01:21:12 +0100

    Load navbar links from pre-seeded JSON

--- palanaeum/frontend/src/components/layout/NavBar.vue
+++ palanaeum/frontend/src/components/layout/NavBar.vue
@@ -2,52 +2,7 @@
   <LinkBar
     :display-icons="vertical"
     :vertical="vertical"
-    :links="[
-      {
-        text: 'Events',
-        icon: 'calendar',
-        url: '/events/'
-      },
-      {
-        text: 'Discover',
-        icon: 'binoculars',
-        children: [
-          { text: 'Recent', url: '/recent/' },
-          { text: 'Tags', url: '/tags/' },
-          { text: 'FAQ', url: '/random/' }
-        ]
-      },
-      {
-        text: 'Contribute',
-        icon: 'pencil',
-        children: [
-          { text: 'Help Transcribe', url: '/events/' },
-          { text: 'Editing Guide', url: '/recent/' },
-          { text: 'Style Guide', url: '/tags/' }
-        ]
-      },
-      {
-        text: 'Links',
-        icon: 'link',
-        children: [
-          { text: 'Chat', url: 'https://discord.gg/DUMHAVV', target: '_blank' },
-          { text: 'Forums', url: 'https://www.17thshard.com/forum/forum/89-arcanum-discussion/', target: '_blank' },
-          { text: 'Wiki', url: 'https://coppermind.net/', target: '_blank' },
-          { text: 'Upcoming Events', url: 'https://www.brandonsanderson.com/upcoming-events/', target: '_blank' }
-        ]
-      },
-      {
-        text: 'Help',
-        icon: 'question-circle',
-        children: [
-          { text: 'User Guide', url: '/events/' },
-          { text: 'Search', url: '/recent/' },
-          { text: 'FAQ', url: '/tags/' },
-          { text: 'About', url: '/tags/' },
-          { text: 'Contact', url: '/tags/' }
-        ]
-      }
-    ]"
+    :links="links"
   />
 </template>
 
@@ -61,6 +16,11 @@ export default {
     vertical: {
       type: Boolean,
       default: () => false
+    }
+  },
+  computed: {
+    links () {
+      return window.Palanaeum.NAVBAR_LINKS
     }
   }
 }
