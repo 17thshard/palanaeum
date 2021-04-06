be0e30783a958cb83b051e7cbe2a4424849a8e90
Maciej Strzelczyk <strzelczyk@google.com>
2021-04-06 14:28:25 +0200

    Fixing API homepage links.

--- palanaeum/api/urls.py
+++ palanaeum/api/urls.py
@@ -4,9 +4,9 @@ from palanaeum.api.views import EventViewSet, TagsViewSet, SearchEntryViewSet, R
     UpcomingEventsViewSet, EntryViewSet
 
 api_router = routers.DefaultRouter()
-api_router.register('events', EventViewSet)
-api_router.register('entry', EntryViewSet)
-api_router.register('upcoming_events', UpcomingEventsViewSet)
+api_router.register('events', EventViewSet, basename='events')
+api_router.register('upcoming_events', UpcomingEventsViewSet, basename='upcoming_events')
 api_router.register('tags', TagsViewSet)
-api_router.register('search_entry', SearchEntryViewSet)
-api_router.register('random_entry', RandomEntryViewSet)
+api_router.register('entry', EntryViewSet, basename='entry')
+api_router.register('search_entry', SearchEntryViewSet, basename='search_entry')
+api_router.register('random_entry', RandomEntryViewSet, basename='random_entry')
